7ffbc4b9ab40365516d5e01937a2a4960bffa927
niconap <nico.nap@outlook.com>
2020-07-27 11:55:35 +0200

    Make gameboard interactive.

--- scripts.js
+++ scripts.js
@@ -3,27 +3,54 @@ let middleRow = document.getElementById("middle");
 let bottomRow = document.getElementById("bottom");
 
 const Gameboard = () => {
-    let board = ["o", "x", "x", "x", "o", "x", "o", "x", "o"];
+    let board = [" ", "x", " ", "x", "o", "x", "o", " ", "o"];
     
-    let render = (board) => {
+    let render = () => {
+        topRow.innerHTML = "";
+        middleRow.innerHTML = "";
+        bottomRow.innerHTML = "";
         for (let i = 0; i < 3; i++) {
             let cell = topRow.insertCell();
+            cell.setAttribute("data-id", i);
+            cell.addEventListener("click", getId = () => {
+                let cellId = cell.getAttribute("data-id");
+                changeState(cellId);
+            });
             cell.innerHTML = board[i];
         }
         for (let i = 3; i < 6; i++) {
             let cell = middleRow.insertCell();
+            cell.setAttribute("data-id", i);
+            cell.addEventListener("click", getId = () => {
+                let cellId = cell.getAttribute("data-id");
+                changeState(cellId);
+            });
             cell.innerHTML = board[i];
         }
         for (let i = 6; i < 9; i++) {
             let cell = bottomRow.insertCell();
+            cell.setAttribute("data-id", i);
+            cell.addEventListener("click", getId = () => {
+                let cellId = cell.getAttribute("data-id");
+                changeState(cellId);
+            });
             cell.innerHTML = board[i];
         }
+        let cells = document.querySelector("td");
     }
-    
+
+    let changeState = (id) => {
+        if ()
+        board[id] = "x";
+        render();
+    };
+
     return {render};
 };
 
-const Player = (name) => {
+const Player = (name, shape) => {
     const getName = name;
-    return {getName};
+    const getShape = shape;
+
+    return {getName, getShape};
 };
